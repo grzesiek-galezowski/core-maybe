4bc9ab550ae1eab7531331c7e686af30bfaa9b8c
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-01-20 15:56:12 +0100

    Add NRT extensions

---
+++ Core.Maybe.NTests/NullableReferenceTypeExtensionsSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.NullableReferenceTypesExtensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Core.Maybe.Tests;
+
+public class NullableReferenceTypeExtensionsSpecification
+{
+    [Test]
+    public async Task METHOD()
+    {
+        (await Task.FromResult("")).OrThrow();
+        await (Task.FromResult<string?>("").OrThrowAsync());
+        await this.Awaiting(_ => Task.FromResult<string?>(null).OrThrowAsync())
+            .Should().ThrowExactlyAsync<InvalidOperationException>()
+            .WithMessage("Could not convert instance of type System.String? to non-nullable reference type because it is null");
+        await Task.FromResult("").OrThrow();
+        this.Invoking(_ => (null as string).OrThrow())
+            .Should().ThrowExactly<InvalidOperationException>()
+            .WithMessage("Could not convert instance of type System.String? to non-nullable reference type because it is null");
+    }
+}
---
+++ Core.Maybe/NullableReferenceTypesExtensions/BasicNullableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Core.NullableReferenceTypesExtensions;
+
+public static class BasicNullableExtensions
+{
+    public static T OrThrow<T>(this T? instance)
+    {
+        return instance.OrThrow(nameof(instance));
+    }
+
+    public static T OrThrow<T>(this T? instance, string instanceName)
+    {
+        return instance ??
+               throw new InvalidOperationException(
+                   $"Could not convert {instanceName} of type {typeof(T)}? " +
+                   "to non-nullable reference type because it is null");
+    }
+
+    public static async Task<T> OrThrowAsync<T>(this Task<T?> instance)
+    {
+        return (await instance).OrThrow();
+    }
+
+    public static async Task<T> OrThrowAsync<T>(this Task<T?> instance, string instanceName)
+    {
+        return (await instance).OrThrow(instanceName);
+    }
+}
