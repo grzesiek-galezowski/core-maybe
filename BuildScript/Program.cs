f0f414ce71118e33fa99c880644a2c761718bee3
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-02-06 19:22:25 +0100

    Preliminary build script

---
+++ BuildScript/Program.cs
@@ -0,0 +1,86 @@
+﻿using AtmaFileSystem;
+using AtmaFileSystem.IO;
+using FluentAssertions;
+using NScan.Adapters.Secondary.NotifyingSupport;
+using NScan.SharedKernel.WritingProgramOutput.Ports;
+using TddXt.NScan;
+using static Bullseye.Targets;
+using static DotnetExeCommandLineBuilder.DotnetExeCommands;
+using static SimpleExec.Command;
+
+const string configuration = "Release";
+const string version = "2.3.1";
+
+// Define directories.
+var root = AbsoluteFilePath.OfThisFile().ParentDirectory(1).Value;
+var nugetPath = root.AddDirectoryName("NuGets").AddDirectoryName(configuration);
+
+//////////////////////////////////////////////////////////////////////
+// TASKS
+//////////////////////////////////////////////////////////////////////
+
+Target("Clean", () =>
+{
+  Run("dotnet", Clean().Configuration(configuration),
+    workingDirectory: root.ToString());
+});
+
+Target("Build", () =>
+{
+  Run("dotnet",
+    Build()
+      .Configuration(configuration)
+      .WithArg($"-p:VersionPrefix={version}")
+      .WithArg($"-p:SymbolPackageFormat=snupkg"),
+    workingDirectory: root.ToString());
+
+});
+
+Target("NScan", DependsOn("Build"), () =>
+{
+  NScanMain.Run(
+    new InputArgumentsDto
+    {
+      RulesFilePath = AbsoluteDirectoryPath.OfThisFile().AddFileName("rules.txt").AsAnyFilePath(),
+      SolutionPath = root.AddFileName("Core.Maybe.sln").AsAnyFilePath()
+    },
+    new ConsoleOutput(),
+    new ConsoleSupport(Console.WriteLine)
+  ).Should().Be(0);
+});
+
+Target("Test", DependsOn("NScan"), () =>
+{
+  Run("dotnet",
+    Test()
+      .NoBuild()
+      .Configuration(configuration)
+      .WithArg($"-p:VersionPrefix={version}"),
+    workingDirectory: root.ToString());
+});
+
+Target("Push", DependsOn("Clean", "Test"), () =>
+{
+  foreach (var nupkgPath in nugetPath.GetFiles("*.nupkg"))
+  {
+    Run("dotnet", NugetPush(nupkgPath).Source("https://api.nuget.org/v3/index.json"));
+  }
+});
+
+Target("default", DependsOn("Test"));
+
+RunTargetsAndExit(args);
+
+public class ConsoleOutput : INScanOutput
+{
+  public void WriteAnalysisReport(string analysisReport)
+  {
+    Console.WriteLine(analysisReport);
+  }
+
+  public void WriteVersion(string coreVersion)
+  {
+    Console.WriteLine(coreVersion);
+  }
+}
+
