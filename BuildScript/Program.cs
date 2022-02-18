11189c69a8c47ac1abf22e7310be6692e8177b37
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-02-18 08:55:17 +0100

    v2.4.0

--- BuildScript/Program.cs
+++ BuildScript/Program.cs
@@ -9,7 +9,7 @@ using static DotnetExeCommandLineBuilder.DotnetExeCommands;
 using static SimpleExec.Command;
 
 const string configuration = "Release";
-const string version = "2.3.1";
+const string version = "2.4.0";
 
 // Define directories.
 var root = AbsoluteFilePath.OfThisFile().ParentDirectory(1).Value;
