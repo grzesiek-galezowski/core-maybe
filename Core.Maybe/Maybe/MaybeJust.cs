9295ede9780032e5f3b90d3624790f2986a27c58
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-02-20 19:11:05 +0100

    Reverted caller expression as non-backward compatible

--- Core.Maybe/Maybe/MaybeJust.cs
+++ Core.Maybe/Maybe/MaybeJust.cs
@@ -1,35 +1,23 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace Core.Maybe
 {
   public static class MaybeJust
   {
-    public static Maybe<T> Just<T>(this T? value, [CallerArgumentExpression("value")] string valueOrigin = "")
-      where T : struct
+    public static Maybe<T> Just<T>(this T? value) where T : struct
     {
       if (value.HasValue)
-      {
         return value.Value.ToMaybe();
-      }
-
-      throw new ArgumentNullException(nameof(value),
-        $"Cannot create a Just<{typeof(T)}>, because {ValueOrigin(valueOrigin, nameof(value))} is null");
+      throw new ArgumentNullException(nameof(value), "Cannot create a Just<" + typeof(T) + "> from null");
     }
 
-    public static Maybe<T> Just<T>(this T? value, [CallerArgumentExpression("value")] string valueOrigin = "") where T : notnull
+    public static Maybe<T> Just<T>(this T? value) where T : notnull
     {
       if (value != null)
       {
         return value.ToMaybe();
       }
-      throw new ArgumentNullException(nameof(value), 
-        $"Cannot create a Just<{typeof(T)}>, because {ValueOrigin(valueOrigin, nameof(value))} is null");
-    }
-
-    private static string ValueOrigin(string valueOrigin, string @default)
-    {
-        return valueOrigin != "" ? $"expression {{{valueOrigin}}}" : @default;
+      throw new ArgumentNullException(nameof(value), "Cannot create a Just<" + typeof(T) + "> from null");
     }
   }
 }
--- Core.Maybe/Maybe/MaybeJustAsync.cs
+++ Core.Maybe/Maybe/MaybeJustAsync.cs
@@ -1,23 +1,28 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Core.Maybe
 {
   public static class MaybeJustAsync
   {
-    public static async Task<Maybe<T>> JustAsync<T>(this Task<T?> value, [CallerArgumentExpression("value")] string valueOrigin = "")
-      where T : notnull 
-      => (await value).Just(valueOrigin);
+    public static async Task<Maybe<T>> JustAsync<T>(this Task<T?> value) where T : notnull
+    {
+      return (await value).Just();
+    }
 
-    public static async Task<Maybe<T>> JustAsync<T>(this Task<T?> value, [CallerArgumentExpression("value")] string valueOrigin = "") 
-      where T : struct 
-      => (await value).Just(valueOrigin);
+    public static async Task<Maybe<T>> JustAsync<T>(this Task<T?> value) where T : struct
+    {
+      return (await value).Just();
+    }
       
     public static async Task<Maybe<T>> ToMaybeAsync<T>(this Task<T?> value) where T : notnull
-        => (await value).ToMaybe();
+    {
+      return (await value).ToMaybe();
+    }
 
     public static async Task<Maybe<T>> ToMaybeAsync<T>(this Task<T?> value) where T : struct
-        => (await value).ToMaybe();
+    {
+      return (await value).ToMaybe();
+    }
   }
 }
