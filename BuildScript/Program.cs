e2650e1de88e067b6ca67f70b1fa5b9f46402239
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2026-07-05 19:46:14 +0200

    Fixed build script (hanging .NET Host remains, probably a dotnet.exe issue)

--- BuildScript/Program.cs
+++ BuildScript/Program.cs
@@ -14,29 +14,32 @@ const string version = "8.0.0";
 // Define directories.
 var root = AbsoluteFilePath.OfThisFile().ParentDirectory(1).Value();
 var nugetPath = root.AddDirectoryName("NuGets").AddDirectoryName(configuration);
+using var cancellationTokenSource = new CancellationTokenSource();
+var cancellationToken = cancellationTokenSource.Token;
+
 
 //////////////////////////////////////////////////////////////////////
 // TASKS
 //////////////////////////////////////////////////////////////////////
 
-Target("Clean", () =>
+Target("Clean", async () =>
 {
-  Run("dotnet", Clean().Configuration(configuration),
-    workingDirectory: root.ToString());
+  await RunAsync("dotnet", Clean().Configuration(configuration),
+    workingDirectory: root.ToString(), ct: cancellationToken);
   if (nugetPath.Exists())
   {
     nugetPath.Delete(recursive: true);
   }
 });
 
-Target("Build", () =>
+Target("Build", async () =>
 {
-  Run("dotnet",
+  await RunAsync("dotnet",
     Build()
       .Configuration(configuration)
       .WithArg($"-p:VersionPrefix={version}")
       .WithArg($"-p:SymbolPackageFormat=snupkg"),
-    workingDirectory: root.ToString());
+    workingDirectory: root.ToString(), ct: cancellationToken);
 
 });
 
@@ -53,35 +56,35 @@ Target("NScan", dependsOn: ["Build"], () =>
   ).Should().Be(0);
 });
 
-Target("Test", dependsOn: ["NScan"], () =>
+Target("Test", dependsOn: ["NScan"], async () =>
 {
-  Run(
+  await RunAsync(
     "dotnet",
     Test("Core.Maybe.NTests/Core.Maybe.NTests.csproj")
       .NoBuild()
       .Configuration(configuration)
       .WithArg($"-p:VersionPrefix={version}"),
-    workingDirectory: root.ToString());
+    workingDirectory: root.ToString(), ct: cancellationToken);
 });
 
-Target("Pack", dependsOn: ["Clean", "Test"], () =>
+Target("Pack", dependsOn: ["Clean", "Test"], async () =>
 {
-  Run("dotnet", Pack()
+  await RunAsync("dotnet", Pack()
       .NoBuild()
       .Configuration(configuration)
       .WithArg($"-p:VersionPrefix={version}")
       .Output(nugetPath),
-      workingDirectory: root.ToString());
+      workingDirectory: root.ToString(), ct: cancellationToken);
 });
 
-Target("Push", dependsOn: ["Pack"], () =>
+Target("Push", dependsOn: ["Pack"], async () =>
 {
   foreach (var nupkgPath in nugetPath.GetFiles("*.nupkg"))
   {
-    Run("dotnet", NugetPush(nupkgPath).Source("https://api.nuget.org/v3/index.json"));
+    await RunAsync("dotnet", NugetPush(nupkgPath).Source("https://api.nuget.org/v3/index.json"), ct: cancellationToken);
   }
 });
 
 Target("default", dependsOn: ["Test"]);
 
-await RunTargetsAndExitAsync(args);
+await RunTargetsAndExitAsync(args, ex => ex is SimpleExec.ExitCodeException);
